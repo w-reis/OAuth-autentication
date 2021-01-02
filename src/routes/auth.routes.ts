b8ad591499665f953c7ebfeb1806b76cb6f123d8
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-01-02 16:35:44 -0300

    feat(auth):find or create a new user on login
    serialize and deserialize user

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,6 +3,8 @@ import passport from 'passport';
 
 const authRouter = Router();
 
+authRouter.use(passport.initialize());
+
 authRouter.get(
   '/google',
   passport.authenticate('google', {
@@ -10,8 +12,12 @@ authRouter.get(
   })
 );
 
-authRouter.get('/google/redirect', (request, response) => {
-  response.send('You reached the callback URI');
-});
+authRouter.get(
+  '/google/redirect',
+  passport.authenticate('google'),
+  (request, response) => {
+    response.send('You reached the callback URI');
+  }
+);
 
 export default authRouter;
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,16 +1,47 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
+import CreateUserService from './CreateUserService';
+import FindUserService from './FindUserService';
+
 import config from '../config';
 
 const { clientID, clientSecret } = config.auth.google;
 const callbackURL = '/auth/google/redirect';
 
+const createUser = new CreateUserService();
+const findUser = new FindUserService();
+
+passport.serializeUser((user: { id: string }, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id: string, done) => {
+  findUser.execute(id).then(user => done(null, user));
+});
+
 passport.use(
   new GoogleStrategy(
     { clientID, clientSecret, callbackURL },
     (accessToken, refleshToken, profile, done) => {
-      console.log(profile);
+      const {
+        sub: auth_provider_id,
+        picture: avatar,
+        given_name: name,
+        email,
+        // eslint-disable-next-line no-underscore-dangle
+      } = profile._json;
+
+      createUser
+        .execute({
+          auth_provider_id,
+          avatar,
+          name,
+          email,
+        })
+        .then(user => {
+          done(undefined, user);
+        });
     }
   )
 );
