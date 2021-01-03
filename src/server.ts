c534ddae82f180a0cb31f7db79952e3ea566bed0
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-01-03 19:51:36 -0300

    feat(home): add home page view configuration
    set nunjucks as view engine, enable public folder

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,23 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import nunjucks from 'nunjucks';
 import config from './config';
 import routes from './routes';
 import './services/auth';
 import './database';
 
 const app = express();
+
+app.set('view engine', 'njk');
+
+nunjucks.configure('src/views', {
+  express: app,
+  autoescape: false,
+  noCache: true,
+});
+
+app.use(express.static('src/public'));
 
 app.use(routes);
 
